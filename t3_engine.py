1e2f3a2556512c1b289dbf5779d7ce4c57425ec5
jeffresh <jeff.uca@gmail.com>
2020-10-09 14:35:54 +0200

    :sparkles: add class Board to represent the board

--- t3_engine.py
+++ t3_engine.py
@@ -1,8 +1,25 @@
+class Board:
+    def generate_new_board(self, string_board):
+        return [[string_board[x + i] for x in range(3)] for i in range(3)]
+
+    def __init__(self, string_representation):
+        self.board = self.generate_new_board(string_representation)
+
+    def __str__(self):
+        print_board = '-' * 9 + '\n'
+        for i in range(3):
+            print_board += '| ' + ' '.join(self.board[i]) + ' |\n'
+        print_board += '-' * 9
+
+        return print_board
+
+
 class T3Engine:
     def play(self):
-        pass
+        string_board = input('Enter cells: ')
+        self.create_board(string_board)
 
-    def create_board(self):
+    def create_board(self, string_representation):
         pass
 
     def evaluate_board(self):
@@ -19,3 +36,8 @@ class T3Engine:
 
     def evaluate_move(self):
         pass
+
+
+if __name__ == '__main__':
+    new_board = Board('_OOXX____')
+    print(new_board)
