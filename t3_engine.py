544254fb22b0ec9057b9d93fd16812ef8524f746
jeffresh <jeff.uca@gmail.com>
2020-10-09 14:16:07 +0200

    :sparkles: add base files t3_engine.py where the api will be developed tic_tac_toe_engine.py that will be works as the main

---
+++ t3_engine.py
@@ -0,0 +1,21 @@
+class T3Engine:
+    def play(self):
+        pass
+
+    def create_board(self):
+        pass
+
+    def evaluate_board(self):
+        pass
+
+    def do_move(self, player):
+        pass
+
+    def player_move(self):
+        pass
+
+    def cpu_move(self):
+        pass
+
+    def evaluate_move(self):
+        pass
---
+++ tic_tac_toe_engine.py
@@ -0,0 +1,5 @@
+from t3_engine import T3Engine
+
+if __name__ == '__main__':
+    game = T3Engine()
+    game.play()
