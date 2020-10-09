c1ad571cba95a6b4fb6b627e652f97f3dd68fac8
jeffresh <jeff.uca@gmail.com>
2020-10-09 14:59:36 +0200

    :sparkles: add class necessary method to create get, and update the board

--- t3_engine.py
+++ t3_engine.py
@@ -1,9 +1,33 @@
 class Board:
-    def generate_new_board(self, string_board):
-        return [[string_board[x + i] for x in range(3)] for i in range(3)]
 
-    def __init__(self, string_representation):
+    def __init__(self, string_representation=None):
         self.board = self.generate_new_board(string_representation)
+
+    def generate_new_board(self, string_board=None):
+        if string_board:
+            return [[string_board[x + i] for x in range(3)] for i in range(3)]
+        else:
+            return [[' ', ' ', ' '] for _ in range(3)]
+
+    def update_board(self, first_value, second_value, player):
+        coord_i, coord_j = second_value - 1, first_value - 1
+        move_evaluation = self.check_coordinates(coord_i, coord_j)
+
+        if move_evaluation:
+            return False, move_evaluation
+        else:
+            self.board[second_value - 1][first_value - 1] = player
+            return True, self.board
+
+    def check_coordinates(self, coord_i, coord_j):
+        if type(coord_i) != int or coord_j != int:
+            return 'You should enter numbers!'
+        elif coord_i >= len(self.board) or coord_j >= len(self.board[0]):
+            return 'Coordinates should be from 1 to 3!'
+        elif self.board[coord_j][coord_j] != '_' or self.board[coord_j][coord_j]:
+            return 'This cell is occupied! Choose another one!'
+        else:
+            return None
 
     def __str__(self):
         print_board = '-' * 9 + '\n'
